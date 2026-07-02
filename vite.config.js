047241f2d29a343b02ce69cb1a5810b938b2de96
mcurrr <technosigal@gmail.com>
2026-07-02 20:33:07 +0300

    deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Change 'quiz-game' to match your GitHub repository name
-  base: '/quiz-game/'
-})
